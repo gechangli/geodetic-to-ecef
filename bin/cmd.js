0640ab0cc0c2a9beddb2d2d844a946dd9e7af8ce
James Halliday <mail@substack.net>
2015-11-04 18:43:08 -0800

    command-line tool

---
+++ bin/cmd.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+var minimist = require('minimist')
+var ecef = require('../')
+var fs = require('fs')
+var path = require('path')
+
+var argv = minimist(process.argv.slice(2), { alias: { h: 'help' } })
+
+if (argv.help) {
+	usage(0)
+} else if (process.argv.length !== 4) {
+	usage(1)
+} else {
+	var xyz = ecef(process.argv[2], process.argv[3])
+	console.log(xyz.join(' '))
+}
+
+function usage (code) {
+	var r = fs.createReadStream(path.join(__dirname, 'usage.txt'))
+	r.pipe(process.stdout)
+	if (code) r.once('end', function () { process.exit(code) })
+}
