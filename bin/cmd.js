d6bc312725c0507053e6d0607daf417fb5213ea7
James Halliday <mail@substack.net>
2015-11-05 00:49:40 -0800

    wtf tabs

--- bin/cmd.js
+++ bin/cmd.js
@@ -7,16 +7,16 @@ var path = require('path')
 var argv = minimist(process.argv.slice(2), { alias: { h: 'help' } })
 
 if (argv.help) {
-	usage(0)
-} else if (process.argv.length !== 4) {
-	usage(1)
+  usage(0)
+} else if (process.argv.length < 4) {
+  usage(1)
 } else {
-	var xyz = ecef(process.argv[2], process.argv[3])
-	console.log(xyz.join(' '))
+  var xyz = ecef(process.argv[2], process.argv[3], process.argv[4])
+  console.log(xyz.join(' '))
 }
 
 function usage (code) {
-	var r = fs.createReadStream(path.join(__dirname, 'usage.txt'))
-	r.pipe(process.stdout)
-	if (code) r.once('end', function () { process.exit(code) })
+  var r = fs.createReadStream(path.join(__dirname, 'usage.txt'))
+  r.pipe(process.stdout)
+  if (code) r.once('end', function () { process.exit(code) })
 }
